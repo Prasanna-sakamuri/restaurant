6c808909a20348efc13aaf51eb9316607ea200e7
Prasanna-sakamuri <prasannasakamuri19@gmail.com>
2023-03-18 16:04:11 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,247 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/25d81f0704.js" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="template.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <section class="nav" id="nav">
+        <i class="fa-solid fa-utensils"></i>
+        <b>Restro</b>
+        <a id="home" href="#home" target="_self" class="color" id="col">Home</a>
+        <a href="#dishes" target="_self" class="color">Dishes</a>
+        <a href="#about" target="_self" class="color">About</a>
+        <a href="#menu" target="_self" class="color">Menu</a>
+        <a href="#reviews" target="_self" class="color">Reviews</a>
+        <a href="#order" target="_self" class="color">Order</a>
+        <a href="luckt1.html"><i class="fa-sharp fa-solid fa-magnifying-glass"></i></a>
+        <i class="fa-solid fa-heart",href="#"></i>
+        <a href="addtocart.html"><i class="fa-solid fa-cart-shopping"></i></a>
+        <i class="fa-solid fa-bars"></i>
+    </section>
+    <section class="home">
+        <h1 style="text-align: center;">Home</h1>
+        <div class="one">
+            <div>
+                <p>Our special dishes</p>
+                <h1>Spicy Noodles</h1>
+                <pre>Lorem ipsum divi will change the way you build our website forever.
+            The advanced page makes a possible to build truely dynamic
+            pages without learning code.</pre>
+                <button><h4>Order Now</h4></button>
+            </div>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.SH1CYIoX9Ew1H9TIK_MD2gHaFj&pid=Api&P=0" width="500" height="200">
+        </div>
+        <div class="two">
+            <div>
+                <h1>Pizza</h1>
+                <pre>Lorem ipsum divi will change the way 
+            you build our website forever.The advanced page
+            makes a possible to build truely dynamic pages without learning code.</pre>
+                <button><h4>Order Now</h4></button>
+            </div>
+            <img src="https://inspirationseek.com/wp-content/uploads/2016/02/Delicious-Pizza-Pictures.jpg" width="500" height="200">
+        </div>
+        <div class="three">
+            <div>
+                <h1>Biryani</h1>
+                <pre>Lorem ipsum divi will change the way 
+             you build our website forever.The advanced page
+            makes a possible to build truely dynamic pages without learning code.</pre>
+                <button><h4>Order Now</h4></button>
+            </div>
+            <img src="https://www.yoair.com/blog/wp-content/uploads/2021/04/biryani.jpg" width="500" height="200">
+        </div>
+        <div class="four">
+            <div>
+                <h1>Shuarma</h1>
+                <pre>Lorem ipsum divi will change the way 
+            you build our website forever.The advanced page
+            makes a possible to build truely dynamic pages without learning code.</pre>
+                <button><h4>Order Now</h4></button>
+            </div>
+            <img src="https://i.ytimg.com/vi/87oYnupQPVY/maxresdefault.jpg" width="500" height="200">
+        </div>
+    </section>
+    <h1 style="text-align:center">Our dishes</h1>
+<section class="dishes" id="dishes">
+    <div class="dishes1">
+        <div>
+        <img src="https://richmeats.capetown/wp-content/uploads/2017/03/Cheese-Burger-1.jpg" width="400" height="400">
+        <h2 >Tasty food </h2>
+        <h4  style="display:inline-block;">$15.99</h4>
+        <button style="display:inline-block;padding: 2%;">Add to cart</button>
+        </div>
+        <div>
+        <img src="http://www.blog.sagmart.com/wp-content/uploads/2015/04/Tandoori-Chicken.jpg" width="400" height="400">
+        <h2>Tasty food </h2>
+        <h4 style="display:inline-block;">$15.99</h4>
+        <button  style="display:inline-block;padding: 2%;">Add to cart</button>
+        </div>
+        <div>
+        <img src="http://1.bp.blogspot.com/-hA0jswPUXN4/TnDdUUbHynI/AAAAAAAABDs/D6bSTVS0dXg/s1600/French-Fries+3.jpg" width="400" height="400">
+        <h2>Tasty food </h2>
+        <h4 style="display:inline-block;">$15.99</h4>
+        <button style="display:inline-block;padding:2%">Add to cart</button>
+        </div>
+    </div>
+    <div class="dishes2">
+        <div>
+        <img src="https://tse4.mm.bing.net/th?id=OIP.dEyEvqfGH62TNJfiOrLy2gHaE8&pid=Api&P=0" width="400" height="400">
+        <h2>Tasty food </h2>
+        <h4 style="display:inline-block" >$15.99</h4>
+        <button style="display:inline-block;padding:2%">Add to cart</button>
+        </div>
+        <div>
+        <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/lw6c3swncf1ymrgyuhzo" width="400" height="400">
+        <h2>Tasty food </h2>
+        <h4 style="display:inline-block">$15.99</h4>
+        <button style="display:inline-block;padding:2%" >Add to cart</button>
+        </div>
+        <div>
+            <img src="https://kreatiewekosidees.files.wordpress.com/2013/06/milkshakes.jpg" width="400" height="400">
+            <h2>Tasty food </h2>
+            <h4 style="display:inline-block">$15.99</h4>
+            <button style="display:inline-block;padding:2%">Add to cart</button>
+            </div>
+    </div>
+</section>
+<h1 style="text-align: center;">Menu</h1>
+<section class="menu" id="menu">
+    <div>
+    <img src="https://inspirationseek.com/wp-content/uploads/2016/02/Delicious-Pizza-Pictures.jpg" width="400" height="300"><br>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half"></i>
+    <h2>Delicious  Food</h2>
+    <pre>Lorem ipsum divi will change the way 
+ you build our website forever.The advanced page
+makes a possible to build truely dynamic pages without learning code.</pre> 
+    <button style="display:inline-block;padding:1%">Add to cart</button>
+    <h4 style="display:inline-block;margin-left: 2%;color: green;">$15.99</h4>
+    </div>
+    <div>
+        <img src="https://www.yoair.com/blog/wp-content/uploads/2021/04/biryani.jpg" width="400" height="300"><br>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star"></i>
+        <i class="fa-solid fa-star-half"></i>
+        <h2>Delicious  Food</h2>
+        <pre>Lorem ipsum divi will change the way 
+     you build our website forever.The advanced page
+    makes a possible to build truely dynamic pages without learning code.</pre> 
+        <button style="display:inline-block;padding:1%">Add to cart</button>
+        <h4 style="display:inline-block;margin-left: 2%;color: green;">$15.99</h4>
+        </div>
+        <div>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.SH1CYIoX9Ew1H9TIK_MD2gHaFj&pid=Api&P=0" width="400" height="300"><br>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star-half"></i>
+            <h2>Delicious  Food</h2>
+            <pre>Lorem ipsum divi will change the way 
+         you build our website forever.The advanced page
+        makes a possible to build truely dynamic pages without learning code.</pre> 
+            <button style="display:inline-block;padding:1%">Add to cart</button>
+            <h4 style="display:inline-block;margin-left: 2%;color: green;">$15.99</h4>
+            </div>
+</section>
+<h1 style="text-align: center;">Reviews</h1>
+<section class="review" id="reviews">
+    <div>
+    <img src="http://images.amcnetworks.com/amc.com/wp-content/uploads/2016/11/23214_HUMANS_S2_Mia_Is_A_Synthetic_10-800x450.jpg" width="200" height="200">
+    <h3>Ram Pothineni</h3>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half"></i>
+    <pre>“I was so impressed with my breakfast this morning! 
+    I tried the Fried Green Tomato Benedict and my boyfriend got
+    the Crab Cakes Benedict. We both finished our whole plates 
+    and were so impressed with the quality of the food and the 
+    short amount of time it took to receive it. Our waitress
+     was sweet and helpful.
+    I will definitely be back!” </pre>
+    </div>
+    <div>
+    <img src="https://starsunfolded.com/wp-content/uploads/2017/04/Ram-Pothineni.jpg" width="200" height="200">
+    <h3>Ram pothineni</h3>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half"></i>
+    <pre>“I was so impressed with my breakfast this morning! 
+    I tried the Fried Green Tomato Benedict and my boyfriend got
+    the Crab Cakes Benedict. We both finished our whole plates 
+    and were so impressed with the quality of the food and the 
+    short amount of time it took to receive it. Our waitress 
+    was sweet and helpful.
+    I will definitely be back!” </pre>
+    </div>
+    <div>
+     <img src="http://1.bp.blogspot.com/-rH9ibyhCVoQ/UNKzT36xWDI/AAAAAAAAASY/oXAzWWb5WWw/s1600/cute+boys+wallpapers+(1).jpg" width="200" height="200">
+     <h3>Nandamuri Tarak</h3>
+     <i class="fa-solid fa-star"></i>
+     <i class="fa-solid fa-star"></i>
+     <i class="fa-solid fa-star"></i>
+     <i class="fa-solid fa-star"></i>
+     <i class="fa-solid fa-star-half"></i>
+     <pre>“I was so impressed with my breakfast this morning! 
+     I tried the Fried Green Tomato Benedict and my boyfriend got
+     the Crab Cakes Benedict. We both finished our whole plates 
+    and were so impressed with the quality of the food and the 
+    short amount of time it took to receive it. Our waitress
+     was sweet and helpful.
+    I will definitely be back!” </pre>
+     </div>
+</section>
+<h1 style="text-align: center;">Order now</h1>
+    <section class="order " id="order">
+        <h3 class="sub heading">free and fast</h3>
+        <form action="">
+            <div class="inputbox">
+                <div class="input">
+                    <span>your name</span>
+                    <input type="text" placeholder="enter ur name" size="65">
+                    <span>your order</span>
+                    <input type="text" placeholder="enter ur order" size="65">
+                    <span>how much</span>
+                    <input type="number" placeholder="how many orders" min="50%" max="150%">
+                    <span>your address</span>
+                    <textarea name="" placeholder="enter ur address" id="" cols="60" rows="10"></textarea>
+                </div>
+                <div class="input1">
+                    <span>your order</span>
+                    <input type="number" placeholder="enter ur number" size="65">
+                    <span>additional food</span>
+                    <input type="text" placeholder="enter with food" size="65">
+                    <span>date nd time</span>
+                    <input type="datetime-local" size="65">
+                    <span>your message</span>
+                    <textarea name="" placeholder="enter ur message" cols="60" rows="10"></textarea>
+                </div>
+            </div>
+        <input type="submit" value="order now" class="btn">
+        </form>
+    </section>
+    <div class="foot">
+        <i class="fa-brands fa-square-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-solid fa-gift"></i>
+    </div>
+</body>
+</html>
